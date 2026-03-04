0f9b2b895cd1c858c35096a5cbd1cb0fae5d0589
Khushbakht Ahmed <khushbakhtahmed13@gmail.com>
2026-03-04 13:39:19 +0500

    Added evaluation functions results for baseline multi-class classification

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.metrics import classification_report, accuracy_score, roc_auc_score, roc_curve
+import numpy as np
+from sklearn.metrics import classification_report, accuracy_score, roc_auc_score, roc_curve, f1_score, confusion_matrix, mean_squared_error, mean_absolute_error, r2_score
 from sklearn.inspection import DecisionBoundaryDisplay
 from matplotlib.colors import ListedColormap
 import matplotlib.patches as mpatches
@@ -66,3 +67,61 @@ def plot_boundary(model, x_train, y_train, x_test, y_test):
 
     plt.tight_layout()
     plt.show()
+
+
+def plot_confusion_matrix(model, x_train, y_train, x_test, y_test, labels):
+
+    #Training Model
+    model.fit(x_train, y_train)
+
+    #Prediciton
+    y_pred = model.predict(x_test)
+
+
+    #Confusion Matrix
+    cm = confusion_matrix(y_test, y_pred, labels = [0,1,2,3,4])
+
+    sns.heatmap(cm,
+                annot = True,
+                fmt = 'd',
+                cmap = "coolwarm",
+                xticklabels = labels,
+                yticklabels = labels)
+
+    plt.xlabel("\nPredicted Label", fontweight = "bold")
+    plt.ylabel("Actual Label", fontweight = "bold")
+    plt.title("Confusion Matrix\n", fontweight="bold")
+
+    plt.tight_layout()
+    plt.show()
+
+
+def get_accuracy(model, x_train, y_train, x_test, y_test):
+
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+
+    print(f"Training Accuracy = {model.score(x_train, y_train):.4f}")
+    print(f"Testing Accuracy = {accuracy_score(y_test, y_pred):.4f}")
+
+def get_macro_f1_score(model, x_train, y_train, x_test, y_test):
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+
+    macro = f1_score(y_test, y_pred, average = "macro")
+    print(f"Macro-F1 Score = {macro:.4f}")
+
+def get_regression_metrics(model, x_train, y_train, x_test, y_test):
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = np.sqrt(mse)
+    mae = mean_absolute_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+
+    print(f"Mean Absolute Error = {mae:.2f}")
+    print(f"Mean Squared Error = {mse:.2f}")
+    print(f"Root Mean Squared Error = {rmse:.2f}")
+    print(f"Training R² = {model.score(x_train, y_train):.2f}")
+    print(f"Testing R² = {r2:.2f}")
